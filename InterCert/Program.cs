26395536e783f868fd509a6e4eddff1ec9b41d15
VLADIMIR GVS89 <gvs89@bk.ru>
2024-01-31 22:27:46 +0300

    intermediate certification

--- InterCert/Program.cs
+++ InterCert/Program.cs
@@ -1,5 +1,44 @@
-﻿
+﻿//Task_1
 
+class Program
+{
+    static void Main()
+    {
+        int M = 1;
+        int N = 10;
+        Console.WriteLine(PrintNaturalNumbers(M, N));
+    }
+
+    static string PrintNaturalNumbers(int M, int N)
+    {
+        if (M >= N)
+            return "";
+        else
+        {
+            Console.WriteLine(M);
+            return PrintNaturalNumbers(M + 1, N);
+        }
+    }
+}
+//Task_2
+class Program
+{
+    static int Ackermann(int m, int n)
+    {
+        if (m == 0)
+            return n + 1;
+        if (n == 0)
+            return Ackermann(m - 1, 1);
+        return Ackermann(m - 1, Ackermann(m, n - 1));
+    }
+
+    static void Main()
+    {
+        Console.WriteLine(Ackermann(3, 4));
+    }
+}
+
+///////////////////Task_3
 int [] CreateArray (int size, int max, int min){
             int [] array  = new int[size];
             Random rand = new();
@@ -29,4 +68,3 @@ int [] ArraySwap(int [] array,int index)
 int [] array = CreateArray(10,10,0);
 System.Console.WriteLine("[" + PrintArray(array) + "]");
 System.Console.WriteLine("[" + PrintArray(ArraySwap(array,array.Length-1)) + "]");
-
